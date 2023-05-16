be11b89f8bf44bc040f7ea46882b82d2fc984079
shayongithub <dat.nguyen190401@vnuk.edu.vn>
2023-05-16 18:53:31 +0700

    Two Sum - 16/05/2023

---
+++ easy/two_sum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    seen = {}
+
+    for index, cur_val in enumerate(nums):
+
+        remaining = target - cur_val
+
+        if remaining in seen:
+            return [index, seen[remaining]]
+        else:
+            seen[cur_val] = index
