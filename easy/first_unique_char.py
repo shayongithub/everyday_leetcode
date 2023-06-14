95ef376d3e06557dba78854ad92b4ab4e2bc66f8
shayongithub <dat.nguyen190401@vnuk.edu.vn>
2023-06-14 22:42:28 +0700

    First Unique Character in String

---
+++ easy/first_unique_char.py
@@ -0,0 +1,32 @@
+def firstUniqChar(s: str) -> int:
+    """
+    Given a string s, find the first non-repeating character in it
+        and return its index. If it does not exist, return -1.
+
+    Approach:
+        - Build a hash-map with the key is the character and the value is the index
+            If we find that a key exists already, we can set it to some value in order to avoid it (like -1)
+        - Filter out keys that appear more than once with value above
+        - Return -1 if the filtered dictionary is empty
+        - Return the minimum value from the dictionary
+
+    """
+    dic = {}
+
+    for ind, char in enumerate(s):
+        if char in dic:
+            dic[char] = -1
+        else:
+            dic[char] = ind
+
+    filterd = dict((k, v) for k, v in dic.items() if v != -1)
+
+    if not filterd:
+        return -1
+
+    return min(filterd.values())
+
+
+if __name__ == "__main__":
+    s = "leetcode"
+    print(firstUniqChar(s))
