1426d24ca61d388d0564467c65c5ced5f153ff6b
shayongithub <dat.nguyen190401@vnuk.edu.vn>
2023-06-24 17:38:14 +0700

    Container With Most Water - 24/06/2023

---
+++ medium/container_with_most_water.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def maxArea(height: List[int]) -> int:
+    max_area = 0
+    left = 0
+    right = len(height) - 1
+
+    while left < right:
+        area = (right - left) * min(height[left], height[right])
+
+        max_area = max(max_area, area)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_area
+
+
+if __name__ == "__main__":
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    print(maxArea(height))
